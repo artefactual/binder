88eaae36df421c4e796ce96e6225f5b114ae7dc1
Mike Cantelon <mcantelon@gmail.com>
2015-03-04 11:05:05 -0800

    Binder migration collides with an AtoM migration.
    Both Binder and AtoM has different migrations that have the same
    number. This should probably be fixed.

--- lib/task/migrate/migrations/arMigration0109.class.php
+++ lib/task/migrate/migrations/arMigration0109.class.php
@@ -18,8 +18,7 @@
  */
 
 /*
- * Add display_standard_id column to information object and information object
- * templates taxonomy and terms
+ * Make uuid column in aip table required and unique
  *
  * @package    AccesstoMemory
  * @subpackage migration
@@ -37,61 +36,56 @@ class arMigration0109
    */
   public function up($configuration)
   {
-    // Add the "Thematic Areas" taxonomy
-    QubitMigrate::bumpTaxonomy(QubitTaxonomy::THEMATIC_AREA_ID, $configuration);
-    $taxonomy = new QubitTaxonomy;
-    $taxonomy->id = QubitTaxonomy::THEMATIC_AREA_ID;
-    $taxonomy->parentId = QubitTaxonomy::ROOT_ID;
-    $taxonomy->name = 'Thematic Area';
-    $taxonomy->note = 'These themes can assist in identifying major collecting areas, but should not be taken as comprehensive. Used as optional access point for ISDIAH repository records.';
-    $taxonomy->culture = 'en';
-    $taxonomy->save();
-
-    // Add the "Geographic Subregions" taxonomy
-    QubitMigrate::bumpTaxonomy(QubitTaxonomy::GEOGRAPHIC_SUBREGION_ID, $configuration);
-    $taxonomy = new QubitTaxonomy;
-    $taxonomy->id = QubitTaxonomy::GEOGRAPHIC_SUBREGION_ID;
-    $taxonomy->parentId = QubitTaxonomy::ROOT_ID;
-    $taxonomy->name = 'Geographic Subregion';
-    $taxonomy->note = 'Geographic subregions, based on local standards/common terminology if available. Used as optional access point for ISDIAH repository records in multi-repository databases.';
-    $taxonomy->culture = 'en';
-    $taxonomy->save();
-
-    // Add the "Thematic Areas" terms
-    foreach (array(
-      array('en' => 'Aboriginal Peoples',                   'fr' => 'Peuples autochtones'),
-      array('en' => 'Agriculture',                          'fr' => 'Agriculture'),
-      array('en' => 'Arts and Culture',                     'fr' => 'Arts et culture'),
-      array('en' => 'Communication',                        'fr' => 'Communication'),
-      array('en' => 'Education',                            'fr' => 'Éducation'),
-      array('en' => 'Environment',                          'fr' => 'Environnement'),
-      array('en' => 'Family / Domestic Life',               'fr' => 'Vie privée'),
-      array('en' => 'Genealogical',                         'fr' => 'Généalogique'),
-      array('en' => 'Geography',                            'fr' => 'Géographie'),
-      array('en' => 'Industry, Manufacturing and Commerce', 'fr' => 'Industries, fabrication, et commerce'),
-      array('en' => 'Labour',                               'fr' => 'Travail'),
-      array('en' => 'Law and Justice',                      'fr' => 'Droit et justice'),
-      array('en' => 'Medicine and Health',                  'fr' => 'Médecine et santé'),
-      array('en' => 'Military',                             'fr' => 'Forces armées'),
-      array('en' => 'Natural Resources',                    'fr' => 'Richesses naturelles'),
-      array('en' => 'Politics and Government',              'fr' => 'Politique et gouvernement'),
-      array('en' => 'Populations',                          'fr' => 'Populations'),
-      array('en' => 'Recreation / Leisure / Sports',        'fr' => 'Loisirs et sports'),
-      array('en' => 'Religion',                             'fr' => 'Religion'),
-      array('en' => 'Science and Technology',               'fr' => 'Sciences et technologie'),
-      array('en' => 'Social Organizations and Activities',  'fr' => 'Vie sociale'),
-      array('en' => 'Transportation',                       'fr' => 'Transport'),
-      array('en' => 'Travel and Exploration',               'fr' => 'Voyages et exploration')) as $termNames)
+    // Remove duplicated aips
+    $uuids = array();
+    foreach (QubitAip::getAll() as $aip)
     {
-      $term = new QubitTerm;
-      $term->parentId = QubitTerm::ROOT_ID;
-      $term->taxonomyId = QubitTaxonomy::THEMATIC_AREA_ID;
-      $term->sourceCulture = 'en';
-      foreach ($termNames as $key => $value) {
-        $term->setName($value, array('culture' => $key));
+      if (in_array($aip->uuid, $uuids))
+      {
+        $criteria = new Criteria;
+        $criteria->addJoin(QubitInformationObject::ID, QubitRelation::OBJECT_ID);
+        $criteria->add(QubitRelation::TYPE_ID, QubitTerm::AIP_RELATION_ID);
+        $criteria->add(QubitRelation::SUBJECT_ID, $aip->id);
+        $criteria->addDescendingOrderByColumn(QubitInformationObject::LFT);
+
+        // Delete related info. objects
+        foreach (QubitInformationObject::get($criteria) as $item)
+        {
+          foreach ($item->digitalObjects as $do)
+          {
+            $do->delete();
+          }
+
+          $item->delete();
+        }
+
+        $aip->delete();
+
+        continue;
       }
-      $term->save();
+
+      $uuids[] = $aip->uuid;
     }
+
+    // Modify column
+    $sql = <<<sql
+
+ALTER TABLE `aip`
+MODIFY COLUMN `uuid` VARCHAR(36) NOT NULL;
+
+sql;
+
+    QubitPdo::modify($sql);
+
+    $sql = <<<sql
+
+ALTER TABLE `aip`
+ADD UNIQUE (`uuid`);
+
+sql;
+
+    QubitPdo::modify($sql);
+
     return true;
   }
 }
